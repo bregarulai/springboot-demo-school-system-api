48cead3ed6f22e66f47887e7109ac37e8919ff4e
Flavio Aquino <bregarulai@gmail.com>
2020-08-04 21:07:29 -0400

    Initial commit

---
+++ springboot-demo-school-system-api/src/main/java/com/bregacode/api/SpringbootDemoSchoolSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.bregacode.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoSchoolSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoSchoolSystemApiApplication.class, args);
+	}
+
+}
---
+++ springboot-demo-school-system-api/src/test/java/com/bregacode/api/SpringbootDemoSchoolSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bregacode.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoSchoolSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
