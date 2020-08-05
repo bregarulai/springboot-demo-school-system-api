934bbd52a05a47e6fd40d7582bb4543a1b01c892
Flavio Aquino <bregarulai@gmail.com>
2020-08-04 21:57:02 -0400

    added curriculum model

---
+++ src/main/java/com/bregacode/api/SpringbootDemoSchoolSystemApi1Application.java
@@ -0,0 +1,13 @@
+package com.bregacode.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoSchoolSystemApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoSchoolSystemApi1Application.class, args);
+	}
+
+}
