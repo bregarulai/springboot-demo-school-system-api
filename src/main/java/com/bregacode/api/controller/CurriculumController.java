324819322495fd1deeabffd2e82439e383efcc16
Flavio Aquino <bregarulai@gmail.com>
2020-08-05 00:36:43 -0400

    Created curriculums end point

--- src/main/java/com/bregacode/api/controller/CurriculumController.java
+++ src/main/java/com/bregacode/api/controller/CurriculumController.java
@@ -1,13 +1,24 @@
 package com.bregacode.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bregacode.api.model.Curriculum;
 import com.bregacode.api.service.CurriculumService;
 
 @RestController
+@RequestMapping("/api")
 public class CurriculumController {
 	@Autowired
 	CurriculumService service;
+	
+	@GetMapping("/curriculums")
+	public List<Curriculum> listCurriculums() {
+		return service.listCurriculum();
+	}
 
 }
